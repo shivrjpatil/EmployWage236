2ae9c0cc7e3711e16e0391a9b794a26c60787a41
shivrjpatil <shivrajp972@gmail.com>
2022-12-19 11:59:16 +0530

    adde a welcome message

---
+++ EmployeeWageProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Employeewage Computation");
+            Console.ReadLine();
+        }
+    }
+}
